20522d69ddf595129d0697e8869f4b51c7190154
Olivia Coffey <livvycoffey@gmail.com>
2018-10-02 13:04:23 -0400

    test more

--- gpull.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#the purpose of this script is to do a git pull
-#on all local repos at system startup
-cd ~ && cd Documents
-for d in Github/*/
-do
-    (cd "$d" && git pull)
-done
